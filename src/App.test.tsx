368f4ed53857a017c2df0071ec2965593ec41ab1
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-07-04 13:46:39 +0200

    first tp

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,19 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('<App />', () => {
+  const renderAppComponent = () => {
+    render(<App />);
+    return {
+      h1: screen.getByRole('heading')
+    }
+  }
+  it('should be disabled by default', () => {
+    const { h1  } = renderAppComponent();
+    expect(h1).toHaveTextContent('Hello World')
+    expect(screen.getByText("Bulma")).toBeInTheDocument();
+  });
+})
+
+
+
