bbd44e6dae76b4ef6e7dba24d16c467f2a54f5da
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-07-11 10:27:10 +0200

    feat: end redux

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,21 @@
 import { PayloadAction, configureStore, createSlice } from '@reduxjs/toolkit'
 
+type CounterState = {
+    people: any[]
+}
+
 const counter = createSlice({
     name: 'counter',
     initialState: {
         people: []
     },
     reducers: {
-        like(state, action: PayloadAction<any>){
-
+        like(state: CounterState, action: PayloadAction<any>){
+            state.people.push(action.payload)
         },
         dislike(state, action: PayloadAction<any>){
-
+            const index = state.people.findIndex((p: any) => p.name === action.payload.name)
+            state.people.splice(index, 1)
         }
     }
 })
@@ -23,10 +28,17 @@ const store = configureStore({
   },
 })
 
+
+export const { like, dislike } = counter.actions;
+
 export type RootState = ReturnType<typeof store.getState>
 
 export const getAllLikesCount = (state: RootState) => {
     return state.counter.people.length;
+}
+
+export const isLiked = (state: RootState, person: any): boolean => {
+    return state.counter.people.some((p: any) => p.name === person.name)
 }
 
 export default store
